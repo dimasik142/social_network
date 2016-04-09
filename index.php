28208c8ccf7264148338a4a343891ea8c24e2e76
dimasik142 <ivanov.dnytro.ua@gmail.com>
2016-04-09 12:44:20 +0300

    new_classes

--- index.php
+++ index.php
@@ -1,77 +1,9 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Home
- * Date: 02.04.2016
- * Time: 12:58
- */
-class User
-{
-    private $name = "Олена";
-    private $surename = "Телеш";
-    private $sex = "girl";
-    private $activity = true;
-    private $city = "Київ";
-
-    private $date_of_birth = array(
-        "data" => 5,
-        "month" => "03",
-        "year" => 1997
-    );
-
-    private $films = array(
-        '0'=>"Spiderman",
-        '1'=>"Superman",
-        '2'=>"Aroneman",
-        '3'=>"Speadman",
-        '4'=>"Lolman"
-    );
-
-    function get_age(){
-        return (2016 - $this->date_of_birth["year"]);
-    }
-
-    function set_information(){
-        echo'
-        Місто:          '.$this->city.'           </p>
-        Дата народження:    '.$this->date_of_birth["data"].'.'.$this->date_of_birth["month"].'.'.$this->date_of_birth["year"].' ( '.$this->get_age().'  років )                        </p>
-        Улюблені фільми: ';
-        $this->set_films();
-    }
-
-    function set_films(){
-        echo "  ";
-        for ($i = 0;$i<count($this->films);$i++){
-            echo $this->films[$i];
-            if ($i != count($this->films)-1)
-                echo " , ";
-        }
-    }
-
-    function set_name(){
-        if ($this->activity)
-            echo'<div class ="title">                              '.$this->name.' '.$this->surename.'                                                                                      		 Online</div>';
-        else
-            echo'<div class ="title">                              '.$this->name.' '.$this->surename.'                                                                                  			 </div>';
-    }
-
-    function set_photo(){
-        $photo_file = "photo/lena3.jpg";
-        $imsize = getimagesize($photo_file);
-        $height = $imsize[1];
-        $width = $imsize[0];
-        if ($width < 250) {
-            echo '<p id ="lena"><img src="photo/lena3.jpg" id ="photo_low_quality"></p>';
-        }
-        else
-            echo '<p id ="lena"><img src="photo/lena3.jpg" id ="photo"></p>';
-    }
-}
-?>
 
 <?php
+    include 'User_class.php';
     $user = new User();
 ?>
+
 <!DOCTYPE html>
 <html >
 <head>
@@ -113,7 +45,7 @@ class User
                 <li><a href="biografi.php">Друзі</a></li>
                 <li><a href="mgu.php">Повідомлення</a></li>
                 <li><a href="photo.php">Спільноти</a></li>
-                <li><a href="bibliotek.php">Новини</a></li>
+                <li><a href="registration.php">Новини</a></li>
                 <li><a href="forum.php">Налаштування</a></li>
             </ul>
         </nav>
