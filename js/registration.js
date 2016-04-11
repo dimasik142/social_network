66cfb2ab6853cdf7101f10b7b26cddb4e4db3d12
dimasik142 <ivanov.dnytro.ua@gmail.com>
2016-04-11 17:02:24 +0300

    new file

---
+++ js/registration.js
@@ -0,0 +1,7 @@
+function chenge_password(){
+    if (document.getElementById("pass") != document.getElementById("pass_repeat")){
+        alert("паролы не спывпадають");
+    }
+
+
+}
---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html >
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title> Вхід </title>
+    <link rel="stylesheet" href="styles/registration1.css" />
+</head>
+<body>
+<div class="container">
+    <div class="login">
+    <h1>Вхід<h1> </p><br>
+      <form class="bl_form">
+       <input type="email" name="User_email" size="15" maxlength="40" required placeholder="Eлектронна скринька"></p><br>
+       <input type="password" name="User_password" size="15" maxlength="40" required placeholder="Пароль"></p><br>
+       <input type="submit" class="login_button" value="Вхід"></p><br>
+       <input type="button" class="other_button"  onClick='location.href="registration.html"' value="Реєстрація" >
+       </form>
+    </div>
+</div>
+</body>
+
+<?php
+
+?>
