4d6e643ab7423004ace5212b57994bf3f641a337
dimasik142 <ivanov.dnytro.ua@gmail.com>
2016-04-16 01:36:00 +0300

    16/04

--- User_class.php
+++ User_class.php
@@ -1,6 +1,7 @@
 <?php
 class User
 {
+    private $id = 0;
     private $name = "Олена";
     private $surename = "Телеш";
     private $sex = "girl";
@@ -16,58 +17,90 @@ class User
     );
 
     private $films = array(
-        '0'=>"Spiderman",
-        '1'=>"Superman",
-        '2'=>"Aroneman",
-        '3'=>"Speadman",
-        '4'=>"Lolman"
+        '0' => "Spiderman",
+        '1' => "Superman",
+        '2' => "Aroneman",
+        '3' => "Speadman",
+        '4' => "Lolman"
     );
 
-    function get_age(){
+    function get_age()
+    {
         return (2016 - $this->date_of_birth["year"]);
     }
 
-    function get_information_by_registration($user_name,$user_surename,$user_email,$user_password){
+    function get_information_by_registration($user_name, $user_surename, $user_email, $user_password)
+    {
         $this->name = $user_name;
         $this->surename = $user_surename;
         $this->email = $user_email;
         $this->password = $user_password;
     }
 
-    function set_information(){
-        echo'
-        Місто:          '.$this->city.'           </p>
-        Дата народження:    '.$this->date_of_birth["data"].'.'.$this->date_of_birth["month"].'.'.$this->date_of_birth["year"].' ( '.$this->get_age().'  років )                        </p>
+    function set_information()
+    {
+        echo '
+        Місто:          ' . $this->city . '           </p>
+        Дата народження:    ' . $this->date_of_birth["data"] . '.' . $this->date_of_birth["month"] . '.' . $this->date_of_birth["year"] . ' ( ' . $this->get_age() . '  років )                        </p>
         Улюблені фільми: ';
         $this->set_films();
     }
 
-    function set_films(){
+    function set_films()
+    {
         echo "  ";
-        for ($i = 0;$i<count($this->films);$i++){
+        for ($i = 0; $i < count($this->films); $i++) {
             echo $this->films[$i];
-            if ($i != count($this->films)-1)
+            if ($i != count($this->films) - 1)
                 echo " , ";
         }
     }
 
-    function set_name(){
+    function set_name()
+    {
         if ($this->activity)
-            echo'<div class ="title">                              '.$this->name.' '.$this->surename.'                                                                                      		 Online</div>';
+            echo '<div class ="title">                              ' . $this->name . ' ' . $this->surename . '                                                                                      		 Online</div>';
         else
-            echo'<div class ="title">                              '.$this->name.' '.$this->surename.'                                                                                  			 </div>';
+            echo '<div class ="title">                              ' . $this->name . ' ' . $this->surename . '                                                                                  			 </div>';
     }
 
-    function set_photo(){
+    function set_photo()
+    {
         $photo_file = "photo/lena3.jpg";
         $imsize = getimagesize($photo_file);
         $height = $imsize[1];
         $width = $imsize[0];
         if ($width < 250) {
             echo '<p id ="lena"><img src="photo/lena3.jpg" id ="photo_low_quality"></p>';
-        }
-        else
+        } else {
             echo '<p id ="lena"><img src="photo/lena3.jpg" id ="photo"></p>';
         }
     }
+
+    private function connect_bd(){
+        $connection = new mysqli('localhost', 'root', '', 'User');
+        if ($connection->connect_error) {
+            die('Connect Error :' . $connection->connect_error);
+        }
+        return $connection;
+    }
+
+    function get_information_from_db()
+    {
+        $connect = $this->connect_bd();
+        $sql = "SELECT * FROM Information";
+        if ($res = $connect->query($sql)) {
+            if ($res->num_rows > 0) {
+                $information_array = $res->fetch_all(MYSQLI_ASSOC);
+                foreach($information_array as $item):
+                    $this->name = $item['name'];
+                    $this->surename = $item['surename'];
+                    $this->sity = $item['sity'];
+                    $this->email = $item['email'];
+                    $this->password = $item['password'];
+                endforeach;
+            }
+        }
+    }
+}
 ?>
--- login.php
+++ login.php
@@ -9,12 +9,12 @@
 <body>
 <div class="container">
     <div class="login">
-    <h1>Вхід<h1> </p><br>
+    <h1>Вхід</h1> </p><br>
       <form class="bl_form">
        <input type="email" name="User_email" size="15" maxlength="40" required placeholder="Eлектронна скринька"></p><br>
        <input type="password" name="User_password" size="15" maxlength="40" required placeholder="Пароль"></p><br>
-       <input type="submit" class="login_button" value="Вхід"></p><br>
-       <input type="button" class="other_button"  onClick='location.href="registration.html"' value="Реєстрація" >
+       <input type="button" class="login_button" onclick="check_email_and_password_js()" value="Вхід"></p><br>
+       <input type="button" class="other_button"  onclick='location.href="registration.html"' value="Реєстрація" >
        </form>
     </div>
 </div>
@@ -22,4 +22,105 @@
 
 <?php
 
+function connect_bd(){
+    $connection = new mysqli('localhost', 'root', '', 'User');
+    if ($connection->connect_error) {
+        die('Connect Error :' . $connection->connect_error);
+    }
+    return $connection;
+}
+
+function check_email_and_password_php($User_email , $User_password){
+    $connect = connect_bd();
+    $sql = "SELECT * FROM Information";
+    if ($res = $connect->query($sql)) {
+        if ($res->num_rows > 0) {
+            $information_array = $res->fetch_all(MYSQLI_ASSOC);
+            foreach($information_array as $item ):
+                $email = $item['email'];
+                $password = $item['password'];
+                if(($email == $User_email) and ($password == $User_password)){
+                    setcookie('email', $email);
+                    setcookie('password', $password);
+                    return 0;
+                }
+            endforeach;
+            return 1;
+        }
+    }
+}
+
+
+
+/*   function check_email_and_password_php(){
+       $connect = connect_bd();
+       $User_email = $_POST['User_email'];
+       $User_password = $_POST['User_password'];
+       $sql = "SELECT * FROM Information";
+       if ($res = $connect->query($sql)) {
+           if ($res->num_rows > 0) {
+               $information_array = $res->fetch_all(MYSQLI_ASSOC);
+
+               foreach($information_array as $item ):
+                   $email = $item['email'];
+                   $password = $item['password'];
+                   if(($email == $User_email) and ($password == $User_password)){
+                       setcookie('email', $email);
+                       setcookie('password', $password);
+                       require('index.php');
+                       return 0;
+                   }
+               endforeach;
+                   require('login.php');
+           }
+       }
+   }*/
 ?>
+
+
+
+<script type="text/javascript">
+
+    function getXmlHttp(){
+        var xmlhttp;
+        try {
+            xmlhttp = new ActiveXObject("Msxml2.XMLHTTP");
+        } catch (e) {
+            try {
+                xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+            } catch (E) {
+                xmlhttp = false;
+            }
+        }
+        if (!xmlhttp && typeof XMLHttpRequest!='undefined') {
+            xmlhttp = new XMLHttpRequest();
+        }
+        return xmlhttp;
+    }
+
+    function check_email_and_password_js() {
+        alert("Зайшли в check_email_and_password_js ");
+        var req = getXmlHttp();
+        var email = document.getElementsByName('User_email');
+        var password = document.getElementsByName('User_password');
+        var check_data = check_email_and_password_php(email.value,password.value);
+        alert("Пройшли check_email_and_password_php");
+        req.onreadystatechange = function() {
+            if (req.readyState == 4) {
+                if(req.status == 200  ) {
+                    if ( check_data == 1) {
+                        alert("Пароль введено не вірно. Повторіть будь ласка ввід паролю.");
+                        email.value = "";
+                        password.value = ""
+                    }
+                    else{
+                        document.location.href = "my_page.php";
+                    }
+                }
+            }
+        };
+        alert("Вийшли з check_email_and_password_js ");
+        req.open('GET', 'vote.php', true);
+        req.send(null);
+    }
+</script>
